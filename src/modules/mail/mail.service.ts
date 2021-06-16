e148fc2043756099803e1e7cffba1cbb311c0cc2
zsevic <zsevic95@gmail.com>
2021-06-16 13:07:08 +0200

    Replace console logs with logger

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,14 +1,16 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { getTestMessageUrl } from 'nodemailer';
 import { isEnvironment } from 'common/utils';
 
 @Injectable()
 export class MailService {
+  private readonly logger = new Logger(MailService.name);
+
   constructor(private readonly mailerService: MailerService) {}
 
   async sendVerificationMail(email: string, token: string): Promise<void> {
-    const url = `http://172.19.241.201:8080/users/verify/${token}`;
+    const url = `http://172.26.203.64:8080/users/verify/${token}`; // TODO replace with client url
 
     const mailInfo = await this.mailerService.sendMail({
       to: email,
@@ -20,9 +22,9 @@ export class MailService {
     });
 
     if (!isEnvironment('production')) {
-      console.log(`Mail preview URL: ${getTestMessageUrl(mailInfo)}`);
+      this.logger.log(`Mail preview URL: ${getTestMessageUrl(mailInfo)}`);
     } else {
-      console.log('Mail is sent');
+      this.logger.log('Mail is sent');
     }
   }
 }
--- src/modules/user/listeners/user-created.listener.ts
+++ src/modules/user/listeners/user-created.listener.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { MailService } from 'modules/mail/mail.service';
 import { UserCreatedEvent } from '../events/user-created.event';
 
 @Injectable()
 export class UserCreatedListener {
+  private readonly logger = new Logger(UserCreatedListener.name);
+
   constructor(private readonly mailService: MailService) {}
 
   @OnEvent('user.created', { async: true })
   async handleUserCreatedEvent(event: UserCreatedEvent): Promise<void> {
-    console.log('sending verification email');
+    this.logger.log('Sending verification email...');
+
     await this.mailService.sendVerificationMail(event.email, event.token);
   }
 }
