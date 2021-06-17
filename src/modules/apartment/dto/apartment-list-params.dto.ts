6e90d16d493814146b09dd81c318391539b152e4
zsevic <zsevic95@gmail.com>
2021-06-18 01:19:00 +0200

    Improve params validation

--- src/modules/apartment/dto/apartment-list-params.dto.ts
+++ src/modules/apartment/dto/apartment-list-params.dto.ts
@@ -1,12 +1,14 @@
 import { Type } from 'class-transformer';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Min } from 'class-validator';
 import { FiltersDto } from 'modules/filter/dto/filters.dto';
 
 export class ApartmentListParamsDto extends FiltersDto {
+  @Min(1)
   @Type(() => Number)
   @IsNotEmpty()
   limitPerPage: number;
 
+  @Min(1)
   @Type(() => Number)
   @IsNotEmpty()
   pageNumber: number;
