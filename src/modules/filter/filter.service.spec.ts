93e75f9a78b9d0fdbd75116ae15f78ac6cc5fdae
zsevic <zsevic95@gmail.com>
2021-08-22 15:20:27 +0200

    Add more tests for filter service

--- src/modules/filter/filter.service.spec.ts
+++ src/modules/filter/filter.service.spec.ts
@@ -1,10 +1,14 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokenService } from 'modules/token/token.service';
 import { RentOrSale } from './filter.enums';
 import { FilterRepository } from './filter.repository';
 import { FilterService } from './filter.service';
 
-const filterRepository = {};
+const filterRepository = {
+  findUnverifiedFilter: jest.fn(),
+};
+
 const tokenService = {
   createAndSaveToken: jest.fn(),
 };
@@ -35,39 +39,56 @@ describe('FilterService', () => {
     filterService = module.get<FilterService>(FilterService);
   });
 
-  it('should return deactivation url by given filter', async () => {
-    const tokenValue = 'token';
-    const filterId = 'id1';
-    const expirationHours = 24;
-    jest.spyOn(tokenService, 'createAndSaveToken').mockResolvedValue({
-      value: tokenValue,
+  describe('getDeactivationUrl', () => {
+    it('should return deactivation url by given filter', async () => {
+      const tokenValue = 'token';
+      const filterId = 'id1';
+      const expirationHours = 24;
+      jest.spyOn(tokenService, 'createAndSaveToken').mockResolvedValue({
+        value: tokenValue,
+      });
+
+      const deactivationUrl = await filterService.getDeactivationUrl(
+        filterId,
+        expirationHours,
+      );
+
+      expect(deactivationUrl).toEqual(
+        `${clientUrl}/filters/deactivation/${tokenValue}`,
+      );
+      expect(tokenService.createAndSaveToken).toHaveBeenCalledWith(
+        { filter: filterId },
+        expirationHours,
+      );
     });
-
-    const deactivationUrl = await filterService.getDeactivationUrl(
-      filterId,
-      expirationHours,
-    );
-
-    expect(deactivationUrl).toEqual(
-      `${clientUrl}/filters/deactivation/${tokenValue}`,
-    );
-    expect(tokenService.createAndSaveToken).toHaveBeenCalledWith(
-      { filter: filterId },
-      expirationHours,
-    );
   });
 
-  it('should return initial filter', () => {
-    const filter = {
-      structures: [1, 2, 0.5, 1.5],
-      municipalities: ['Savski venac', 'Zemun'],
-      furnished: ['semi-furnished'],
-      rentOrSale: RentOrSale.rent,
-      minPrice: 120,
-      maxPrice: 370,
-    };
-    const initialFilter = filterService.getInitialFilter(filter);
+  describe('getInitialFilter', () => {
+    it('should return initial filter', () => {
+      const filter = {
+        structures: [1, 2, 0.5, 1.5],
+        municipalities: ['Savski venac', 'Zemun'],
+        furnished: ['semi-furnished'],
+        rentOrSale: RentOrSale.rent,
+        minPrice: 120,
+        maxPrice: 370,
+      };
+      const initialFilter = filterService.getInitialFilter(filter);
 
-    expect(initialFilter).toEqual({ ...filter, pageNumber: 1 });
+      expect(initialFilter).toEqual({ ...filter, pageNumber: 1 });
+    });
+  });
+
+  describe('verifyAndActivateFilter', () => {
+    it('should throw an error if unverified filter is not found', async () => {
+      const filterId = 'id1';
+      jest
+        .spyOn(filterRepository, 'findUnverifiedFilter')
+        .mockRejectedValue(new BadRequestException());
+
+      await expect(
+        filterService.verifyAndActivateFilter(filterId),
+      ).rejects.toBeInstanceOf(BadRequestException);
+    });
   });
 });
