18cf7d9cd5e597a38d0dbddc351f2942cd2df770
zsevic <zsevic95@gmail.com>
2021-08-26 22:20:07 +0200

    Fix tests

--- src/common/utils/index.spec.ts
+++ src/common/utils/index.spec.ts
@@ -11,7 +11,7 @@ describe('utils', () => {
 
   describe('getSkip', () => {
     it('should return skip number for given page number and limit per page', () => {
-      const skip = getSkip({ limitPerPage: 20, pageNumber: 20 });
+      const skip = getSkip({ limitPerPage: 20, pageNumber: 10 });
 
       expect(skip).toEqual(180);
     });
