8aff94744fd15fbc1d33b7352312ec24b4a50c7a
zsevic <zsevic95@gmail.com>
2021-09-07 05:59:55 +0200

    Remove unused code

--- src/common/config/database.ts
+++ src/common/config/database.ts
@@ -1,9 +1,5 @@
 import { registerAs } from '@nestjs/config';
-import { isEnvironment } from 'common/utils';
 
 export default registerAs('database', () => ({
   url: process.env.DATABASE_URL,
-  MONGODB_URL: isEnvironment('test')
-    ? 'mongodb://localhost:27017/flat-me-testdb'
-    : process.env.MONGODB_URL || 'mongodb://localhost:27017/flat-me-server',
 }));
