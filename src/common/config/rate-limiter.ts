fdad95f869bda3e71035339a743c219cb9677abf
zsevic <zsevic95@gmail.com>
2021-09-12 22:07:58 +0200

    Improve throttling config

--- src/common/config/rate-limiter.ts
+++ src/common/config/rate-limiter.ts
@@ -1,8 +1,10 @@
-export const FILTER_DEACTIVATION_LIMIT = 3;
+import { isEnvironment } from 'common/utils';
+
+export const FILTER_DEACTIVATION_LIMIT = isEnvironment('test') ? Infinity : 3;
 export const FILTER_DEACTIVATION_TTL = 60 * 60 * 24;
 
-export const FILTER_SAVING_LIMIT = 3;
+export const FILTER_SAVING_LIMIT = isEnvironment('test') ? Infinity : 3;
 export const FILTER_SAVING_TTL = 60 * 60 * 24;
 
-export const FILTER_VERIFICATION_LIMIT = 3;
+export const FILTER_VERIFICATION_LIMIT = isEnvironment('test') ? Infinity : 3;
 export const FILTER_VERIFICATION_TTL = 60 * 60 * 24;
