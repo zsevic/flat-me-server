e7527ac7c4d314218db9cfd5ac7cc7b3b2086c50
zsevic <zsevic95@gmail.com>
2021-12-23 00:15:20 +0100

    Improve handling emails in dto

--- src/modules/filter/dto/save-filter.dto.ts
+++ src/modules/filter/dto/save-filter.dto.ts
@@ -1,8 +1,10 @@
+import { Transform } from 'class-transformer';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 import { FilterDto } from './filter.dto';
 
 export class SaveFilterDto extends FilterDto {
   @IsNotEmpty()
   @IsEmail()
+  @Transform((value: string): string => value.toLowerCase())
   email: string;
 }
