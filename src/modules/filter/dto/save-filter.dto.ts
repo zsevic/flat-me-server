9f39f4b6fc8e598c6d0ab6d1b9dc001137d94839
zsevic <zsevic95@gmail.com>
2022-01-24 21:04:34 +0100

    Add plus sign constraint for emails

--- src/modules/filter/dto/save-filter.dto.ts
+++ src/modules/filter/dto/save-filter.dto.ts
@@ -1,8 +1,9 @@
 import { Transform } from 'class-transformer';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, NotContains } from 'class-validator';
 import { FilterDto } from './filter.dto';
 
 export class SaveFilterDto extends FilterDto {
+  @NotContains('+')
   @IsNotEmpty()
   @IsEmail()
   @Transform((value: string): string => value.toLowerCase())
--- src/modules/subscription/subscription.dto.ts
+++ src/modules/subscription/subscription.dto.ts
@@ -1,7 +1,10 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, NotContains } from 'class-validator';
 
 export class Subscription {
+  @NotContains('+')
   @IsNotEmpty()
   @IsEmail()
+  @Transform((value: string): string => value.toLowerCase())
   email: string;
 }
