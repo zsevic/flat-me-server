9865b3bdcf95175fc1f1b56fba4ea79a057c4bec
zsevic <zsevic95@gmail.com>
2022-01-25 23:16:53 +0100

    Restrict subscription by email

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,6 +1,7 @@
 import { ConflictException, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
+import { isEnvironment } from 'common/utils';
 
 @Injectable()
 export class SubscriptionService {
@@ -10,6 +11,8 @@ export class SubscriptionService {
 
   async subscribeByEmail(email: string) {
     try {
+      if (!isEnvironment('production')) return;
+
       await axios({
         url: 'https://api.sendgrid.com/v3/marketing/contacts',
         method: 'PUT',
